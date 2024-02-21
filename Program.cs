340dd392022145ed18d8a9088ce35c293c3a1399
Mikołaj <mikson022@gmail.com>
2024-02-21 15:58:48 +0100

    Restructure app.json and its affiliated classes

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using System.Text.Json.Serialization;
 
 
 
+/*      config/config.json     */
 namespace Rig
 {
     public class Randomx
@@ -99,50 +100,178 @@ namespace Rig
     }    
 }
 
+/*      app.json        */
 namespace AppJSON
 {
-    public class Monero
+    public class NetworkStats
+    {
+        public long difficulty { get; set; }
+        public string? hash { get; set; }
+        public int main_height { get; set; }
+        public int height { get; set; }
+        public long value { get; set; }
+        public int ts { get; set; }
+    }
+    public class MinBlockRewards
+    {
+        [JsonPropertyName("18081")]
+        public int data { get; set; }
+    }
+    public class Price
+    {
+        public double btc { get; set; }
+        public double usd { get; set; }
+        public double eur { get; set; }
+    }
+    public class CurrentEfforts
+    {
+        [JsonPropertyName("18081")]
+        public double data { get; set; }
+    }
+    public class PplnsPortShares
+    {
+        [JsonPropertyName("18081")]
+        public int data { get; set; }
+    }
+    public class PortHash
+    {
+    [JsonPropertyName("18081")]
+        public double data { get; set; }
+    }
+    public class PortMinerCount
+    {
+    [JsonPropertyName("18081")]
+        public int data { get; set; }
+    }
+    public class PortCoinAlgo
+    {
+    [JsonPropertyName("18081")]
+        public string? data { get; set; }
+    }
+    public class PoolStatistics
+    {
+        public double hashRate { get; set; }
+        public int miners { get; set; }
+        public long totalHashes { get; set; }
+        public int lastBlockFoundTime { get; set; }
+        public int lastBlockFound { get; set; }
+        public int totalBlocksFound { get; set; }
+        public int totalMinersPaid { get; set; }
+        public int totalPayments { get; set; }
+        public double roundHashes { get; set; }
+        public int totalAltBlocksFound { get; set; }
+        public int activePort { get; set; }
+        public IList<object>? activePorts { get; set; }
+        public int activePortProfit { get; set; }
+        public MinBlockRewards? minBlockRewards { get; set; }
+        public int pending { get; set; }
+        public Price? price { get; set; }
+        public CurrentEfforts? currentEfforts { get; set; }
+        public PplnsPortShares? pplnsPortShares { get; set; }
+        public double pplnsWindowTime { get; set; }
+        public PortHash? portHash { get; set; }
+        public PortMinerCount? portMinerCount { get; set; }
+        public PortCoinAlgo? portCoinAlgo { get; set; }
+    }
+    public class PoolStats
+    {
+        public IList<string>? pool_list { get; set; }
+        public PoolStatistics? pool_statistics { get; set; }
+        public int last_payment { get; set; }
+    }
+    public class PoolBlock
+    {
+        public object? ts { get; set; }
+        public string? hash { get; set; }
+        public object? diff { get; set; }
+        public object? shares { get; set; }
+        public int height { get; set; }
+        public bool valid { get; set; }
+        public bool unlocked { get; set; }
+        public string? pool_type { get; set; }
+        public object? value { get; set; }
+    }
+    public class PoolPayment
+    {
+        public int id { get; set; }
+        public string? hash { get; set; }
+        public int mixins { get; set; }
+        public int payees { get; set; }
+        public int fee { get; set; }
+        public object? value { get; set; }
+        public object? ts { get; set; }
+        public string? pool_type { get; set; }
+    }
+    public class MinerStats
+    {
+        public double hash { get; set; }
+        public double hash2 { get; set; }
+        public string? identifier { get; set; }
+        public int lastHash { get; set; }
+        public double totalHashes { get; set; }
+        public int validShares { get; set; }
+        public int invalidShares { get; set; }
+        public long amtPaid { get; set; }
+        public long amtDue { get; set; }
+        public int txnCount { get; set; }
+    }
+    public class Global
+    {
+        public int lts { get; set; }
+        public string? identifer { get; set; }
+        public double hash { get; set; }
+        public double hash2 { get; set; }
+        public double totalHash { get; set; }
+        public int validShares { get; set; }
+        public int invalidShares { get; set; }
+    }
+    public class MinerStatsAllworkers
     {
-        public double usd { get; set; }
-        public double eur { get; set; }
-        public double gbp { get; set; }
-        public double chf { get; set; }
-        public double pln { get; set; }
-        public double nok { get; set; }
-        public double tryC { get; set; }
-        public double rub { get; set; }
+        public IDictionary<string, WorkerStats>? Workers { get; set; }
     }
-    public class Nano
+    public class WorkerStats
     {
-        public double usd { get; set; }
-        public double eur { get; set; }
-        public double gbp { get; set; }
-        public double chf { get; set; }
-        public double pln { get; set; }
-        public double nok { get; set; }
-        public double tryC { get; set; }
-        public double rub { get; set; }
+        public int lts { get; set; }
+        public string? identifier { get; set; }
+        public double hash { get; set; }
+        public double hash2 { get; set; }
+        public double totalHash { get; set; }
+        public int validShares { get; set; }
+        public int invalidShares { get; set; }
     }
-    public class Response
-    {
-        public Monero? monero { get; set; }
-        public Nano? nano { get; set; }
-    }
-    public class CoingeckoCom
-    {
-        public string? request { get; set; }
-        public Response? response { get; set; }
-    }
-    public class APIs
-    {
-        public IList<string>? monerodorg { get; set; }
-        public CoingeckoCom? coingeckocom { get; set; }
-    }
-    public class App
-    {
-        public IList<string>? addresses { get; set; }
-        public APIs? APIs { get; set; }
-    }
+    public class MinerPayment
+    {
+        public string? pt { get; set; }
+        public int ts { get; set; }
+        public long amount { get; set; }
+        public string? txnHash { get; set; }
+        public int mixin { get; set; }
+    }
+    public class Response
+    {
+        public NetworkStats? networkStats { get; set; }
+        public IDictionary<string, PoolBlock>? poolBlocks { get; set; } 
+        public IList<string>? minerIdentifiers { get; set; } 
+        public PoolStats? poolStats { get; set; }
+        public IList<PoolPayment>? poolPayments { get; set; }
+        public MinerStats? minerStats { get; set; }
+        public MinerStatsAllworkers? minerStatsAllworkers { get; set; }
+        public IList<MinerPayment>? minerPayments { get; set; }
+    }
+    public class Monerodorg
+    {
+        public IList<string>? request { get; set; }
+        public Response? response { get; set; }
+    }
+    public class APIs
+    {
+        public Monerodorg? monerodorg { get; set; }
+    }
+    public class App
+    {
+        public IList<string>? addresses { get; set; }
+        public APIs? APIs { get; set; }
+    }
 }
 
 
